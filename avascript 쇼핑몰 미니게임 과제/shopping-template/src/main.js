ce4f0bb188bf5db980cf1f0594f03c1906640525
lsy04 <power_brian@naver.com>
2025-07-01 18:49:47 +0900

    JavaScript-handling events

--- avascript 쇼핑몰 미니게임 과제/shopping-template/src/main.js
+++ avascript 쇼핑몰 미니게임 과제/shopping-template/src/main.js
@@ -6,7 +6,6 @@ function loadItems() {
 
 function displayItems(items) {
     const container = document.querySelector('.items');
-    const html = items.map(item => createHTMLString(item))
     container.innerHTML = items.map(item => creatHTMLString(item)).join('');
 }
 
@@ -17,6 +16,27 @@ function creatHTMLString(item) {
       <span class="item__description">${item.gender}, ${item.size}</span>
     </li>
     `;
+}
+
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if(key == null || value == null) {
+        return;
+    }
+    
+    const filtered = items.filter(item => item[key] === value);
+    displayItems(filtered)
+}
+
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo')
+    const buttons = document.querySelector('.buttons')
+    logo.addEventListener('click', () => displayItems(items))
+    buttons.addEventListener('click', event => onButtonClick(event, items))
+
 }
 
 //main
